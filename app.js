40959de0b0e97c8c3152c1ab0946ef5d97021492
himangkydutta <himangkydutta08@gmail.com>
2017-04-27 01:15:20 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+//Declaring and defining variables
+
+var empdetails ={
+					name:"Ankita",
+					age :28,
+					dtofBrth:"2015-03-25",
+					placOfBirth : "Bangalore"
+				};
+
+//Displaying each object property in console
+console.log(empdetails);
+
